5d515392dbfb8021af6e42dc1eebf4a1a2091459
Mohab Ezzat <ezt.mohab10@gmail.com>
2022-12-30 23:51:39 +0200

    Initial commit

---
+++ app/src/main/java/mrandroid/mazaady/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package mrandroid.mazaady.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mrandroid.mazaady.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
