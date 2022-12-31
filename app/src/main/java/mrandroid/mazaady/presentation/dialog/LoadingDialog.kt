5855c779e13a7aed706df49bb8be0fdf9473fcbb
Mohab Ezzat <ezt.mohab10@gmail.com>
2022-12-31 21:32:45 +0200

    submit loading dialog

---
+++ app/src/main/java/mrandroid/mazaady/presentation/dialog/LoadingDialog.kt
@@ -0,0 +1,35 @@
+package mrandroid.mazaady.presentation.dialog
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.view.KeyEvent
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import mrandroid.mazaady.R
+
+object LoadingDialog {
+    private lateinit var dialog: AlertDialog
+    val cancelCurrentRequest = MutableLiveData<Boolean>()
+
+    fun init(activity: Activity) {
+        val view = View.inflate(activity, R.layout.dialog_loading, null)
+        dialog = AlertDialog.Builder(activity, R.style.DialogStyle)
+            .setView(view)
+            .setCancelable(false)
+            .setOnKeyListener { _, keyCode, _ ->
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    cancelCurrentRequest.value = true
+                }
+                true
+            }.create()
+    }
+
+    fun showDialog() {
+        if (!dialog.isShowing) dialog.show()
+    }
+
+    fun dismissDialog() {
+        if (dialog.isShowing) dialog.dismiss()
+    }
+}
